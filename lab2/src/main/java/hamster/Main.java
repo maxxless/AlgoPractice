2b25d1f5868e4182bc912de157a4a220d5a197ee
Max Leshchyshyn <masyaka35@gmail.com>
2018-09-24 10:07:53 +0300

    modified interface declaration, improve algorithm's efficacy by using merge sort

--- lab2/src/main/java/hamster/Main.java
+++ lab2/src/main/java/hamster/Main.java
@@ -2,12 +2,15 @@ package hamster;
 
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     private static int allFood = 0;
     private static int hamstersAmount = 0;
-    private static ArrayList<Hamster> hamsters = new ArrayList<>();
+    private static List<Hamster> hamsters = new ArrayList<>();
+    private static int currentFood = 0;
+    private static int currentGreed = 0;
 
     public static void main(String[] args) {
         openFile();
@@ -20,21 +23,21 @@ public class Main {
             System.out.println(hamster);
         }
 
-        sort(hamsters);
+        int result = searchingForHamster(hamsters, hamstersAmount, allFood);
 
+        mergeSort(hamsters, 0, hamsters.size() - 1);
         System.out.println("\nSorted array:");
 
         for (Hamster hamster : hamsters) {
             System.out.println(hamster);
         }
 
-        int result = searchingForHamster(hamsters, hamstersAmount, allFood);
         System.out.println("\nResult = " + result);
     }
 
     private static void openFile() {
         try {
-            Scanner scanner = new Scanner(new FileReader("hamstr.in"));
+            Scanner scanner = new Scanner(new FileReader("hamstr1.in"));
             while (scanner.hasNext()) {
                 String line = scanner.nextLine();
 
@@ -53,9 +56,7 @@ public class Main {
         }
     }
 
-    static int searchingForHamster(ArrayList<Hamster> hamsters, int hamstersAmount, int allFood) {
-        int currentFood = 0;
-        int currentGreed = 0;
+    static int searchingForHamster(List<Hamster> hamsters, int hamstersAmount, int allFood) {
         int maxAmount = hamstersAmount - 1;
         int lastBiggestIndex;
 
@@ -78,7 +79,7 @@ public class Main {
         return maxAmount;
     }
 
-    private static int count(ArrayList<Hamster> hamsters, int maxAmount) {
+    private static int count(List<Hamster> hamsters, int maxAmount) {
         int conditionalIndex = 0;
         for (int i = 0; i < maxAmount; i++) {
             if ((hamsters.get(conditionalIndex).getFoodNeeded() + hamsters.get(conditionalIndex).getGreed() * maxAmount) < (hamsters.get(i).getFoodNeeded() + hamsters.get(i).getGreed() * maxAmount)) {
@@ -88,16 +89,71 @@ public class Main {
         return conditionalIndex;
     }
 
-    private static void sort(ArrayList<Hamster> hamsters) {
-        Hamster temp;
-        for (int i = 0; i < hamsters.size() - 1; i++) {
-            for (int j = 0; j < hamsters.size() - i - 1; j++) {
-                if (hamsters.get(j).getGreed() < hamsters.get(j + 1).getGreed()) {
-                    temp = hamsters.get(j);
-                    hamsters.set(j, hamsters.get(j + 1));
-                    hamsters.set(j + 1, temp);
-                }
+//    private static void sortBubble(List<Hamster> hamsters) {
+//        Hamster temp;
+//        for (int i = 0; i < hamsters.size() - 1; i++) {
+//            for (int j = 0; j < hamsters.size() - i - 1; j++) {
+//                if (hamsters.get(j).getGreed() < hamsters.get(j + 1).getGreed()) {
+//                    temp = hamsters.get(j);
+//                    hamsters.set(j, hamsters.get(j + 1));
+//                    hamsters.set(j + 1, temp);
+//                }
+//            }
+//        }
+//    }
+
+    private static void merge(List<Hamster> hamsters, int leftIndex, int midIndex, int rightIndex) {
+        int n1 = midIndex - leftIndex + 1;
+        int n2 = rightIndex - midIndex;
+
+        List<Hamster> leftArray = new ArrayList<>();
+        List<Hamster> rightArray = new ArrayList<>();
+
+        for (int i = 0; i < n1; ++i)
+            leftArray.add(i, hamsters.get(leftIndex + i));
+        for (int j = 0; j < n2; ++j)
+            rightArray.add(j, hamsters.get(midIndex + 1 + j));
+
+        int i = 0, j = 0;
+        int k = leftIndex;
+
+        searchingForHamster(leftArray, n1, allFood);
+        if (searchingForHamster(leftArray, n1, allFood) == leftArray.size()) {
+            searchingForHamster(rightArray, n2, allFood);
+        }
+
+        while (i < n1 && j < n2) {
+            if (leftArray.get(i).getGreed() <= rightArray.get(j).getGreed()) {
+                hamsters.set(k, leftArray.get(i));
+                i++;
+            } else {
+                hamsters.set(k, rightArray.get(j));
+                j++;
             }
+            k++;
+        }
+
+        while (i < n1) {
+            hamsters.set(k, leftArray.get(i));
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            hamsters.set(k, rightArray.get(j));
+            j++;
+            k++;
+        }
+    }
+
+    private static void mergeSort(List<Hamster> hamsters, int leftIndex, int rightIndex) {
+        if (leftIndex < rightIndex) {
+            int m = (leftIndex + rightIndex) / 2;
+
+            mergeSort(hamsters, leftIndex, m);
+            mergeSort(hamsters, m + 1, rightIndex);
+
+            merge(hamsters, leftIndex, m, rightIndex);
         }
     }
 }
